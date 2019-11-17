4b7a2df34b270a50d6efdd84dad2aa991a067174
somewheve <somewheve@gmail.com>
2019-11-18 07:49:59 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ packages = find_packages()
 setup(
     author="yutiansut && somewheve",
     name="quantdata",
+    version=0.1,
     description="quant data support of full platform",
     packages=packages,
     install_requires=['pandas','tqsdk','ctpbee'],
